d23e7f3ed803d4911444138ded581bb32e40c9d4
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-03-18 10:11:54 -0300

    Update final da pagina sobre

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-import ScrollSuave from './modules/scroll-suave.js';
 import ScrollAnima from './modules/scroll-anima.js';
 import menuAtivo from './modules/menu-ativo.js';
-
-const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init();
 
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
